abfede247f6e6a42368f50cefd204ada03de7eb6
nandita <nandita.sachdeva@webnyxa.tech>
2023-03-28 18:38:26 +0530

    ratings

--- navigations/MyBookingStack.js
+++ navigations/MyBookingStack.js
@@ -10,6 +10,8 @@ import ThekeForm1 from "../src/ThekeForm1";
 import ThekeParKaam_Form from "../src/ThekeParKaam_Form";
 import SahayakForm from "../src/SahayakForm";
 import MachineBooking from "../src/MachineBooking";
+import Mybooking_Sahayak2 from "../src/Mybooking_Sahayak2";
+import Theke_MachineForm2 from "../src/Theke_MachineForm2";
 // import Homepage from "../src/HomePage";
 
 export default function MyBookingStack () {
@@ -28,8 +30,10 @@ export default function MyBookingStack () {
         <Stack.Screen name="SahayakForm" component={SahayakForm} />
         <Stack.Screen name="MachineBooking" component={MachineBooking} />
         <Stack.Screen name="Theke_MachineForm" component={Theke_MachineForm} />
+        <Stack.Screen name="Theke_MachineForm2" component={Theke_MachineForm2} />
         <Stack.Screen name="MyBook_SahayakForm" component={MyBook_SahayakForm} />
         <Stack.Screen name="Payment" component={Payment} />
+        <Stack.Screen name="Mybooking_Sahayak2" component={Mybooking_Sahayak2} />
 
           
 
