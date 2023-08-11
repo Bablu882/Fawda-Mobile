5f33e63a440e0dcbc17fb10357d2eb11fd494bc0
Piyush Goyal <piyush.goyal@webnyxa.tech>
2023-08-11 18:59:54 +0530

    Enable payment gateway

--- src/Payment.js
+++ src/Payment.js
@@ -190,8 +190,8 @@ export default function Payment({ route, navigation }) {
                 Toast.LONG
               );
             } else if (numBookings === bookingsNumber) {
-              // fetchPaymentHtml();
-              paymentStatus();
+              fetchPaymentHtml();
+              // paymentStatus();
             }
           } else {
             navigation.replace("HomeStack", { screen: "BottomTab" });
@@ -208,8 +208,8 @@ export default function Payment({ route, navigation }) {
             navigation.replace("HomeStack", { screen: "BottomTab" });
             Toast.show("यह बुकिंग सहायक द्वारा रद्द कर दी गई है।", Toast.LONG);
           } else {
-            // fetchPaymentHtml();
-            paymentStatus();
+            fetchPaymentHtml();
+            // paymentStatus();
           }
         } else if (item?.job_type === "machine_malik") {
           const statusCheck =
@@ -222,8 +222,8 @@ export default function Payment({ route, navigation }) {
               Toast.LONG
             );
           } else {
-            // fetchPaymentHtml();
-            paymentStatus();
+            fetchPaymentHtml();
+            // paymentStatus();
           }
         }
       }
