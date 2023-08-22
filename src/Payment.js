79fcf6a1ef5841fe51d0ecc2c286a0432c7c9eb3
Piyush Goyal <piyush.goyal@webnyxa.tech>
2023-08-22 16:23:53 +0530

    Enabe payment gateway

--- src/Payment.js
+++ src/Payment.js
@@ -242,8 +242,8 @@ export default function Payment({ route, navigation }) {
                 Toast.LONG
               );
             } else if (numBookings === bookingsNumber) {
-              // fetchPaymentHtml();
-              paymentStatus();
+              fetchPaymentHtml();
+              // paymentStatus();     // uncomment it will testing
             }
           } else {
             navigation.replace("HomeStack", { screen: "BottomTab" });
@@ -260,8 +260,8 @@ export default function Payment({ route, navigation }) {
             navigation.replace("HomeStack", { screen: "BottomTab" });
             Toast.show("यह बुकिंग सहायक द्वारा रद्द कर दी गई है।", Toast.LONG);
           } else {
-            // fetchPaymentHtml();
-            paymentStatus();
+            fetchPaymentHtml();
+            // paymentStatus();            // uncomment it will testing
           }
         } else if (item?.job_type === "machine_malik") {
           const statusCheck =
@@ -274,8 +274,8 @@ export default function Payment({ route, navigation }) {
               Toast.LONG
             );
           } else {
-            // fetchPaymentHtml();
-            paymentStatus();
+            fetchPaymentHtml();
+            // paymentStatus();                     // uncomment it will testing
           }
         }
       }
